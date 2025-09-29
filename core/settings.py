c67dd39bc3d93a5e8d8f74fbfd589813f880fe08
Marlon Knupp <marlonjcc23@gmail.com>
2025-09-29 13:21:39 -0300

    ...

--- core/settings.py
+++ core/settings.py
@@ -77,13 +77,13 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'sgn',
-        'USER': 'marlonknupp',
-        'PASSWORD': 'Impresora135791*',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
-}
+    }
 
 
 # Password validation
